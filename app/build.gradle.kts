18176d2e40f93f99e7f3e9babc9aa4eef131a510
kongjjj <kongjjj@users.noreply.github.com>
2026-07-10 23:12:35 +0800

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kongjjj.overlay"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.7"
+        versionCode = 8
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
